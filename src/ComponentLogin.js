086b6316ded79a57c128511f70532efd55f53f60
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2018-05-08 17:55:03 -0300

    Estilizado componente login

--- src/ComponentLogin.js
+++ src/ComponentLogin.js
@@ -20,8 +20,16 @@ class ComponentLogin extends Component {
       return (
         <div>
             <span href="#" onClick={this.handleClick}>Login</span>
-            <div className="formLogin " style={!this.state.isDisplay ? { display: 'none' } : {} } > 
-                testes
+
+            <div className="modal-content formLogin" role="document" style={!this.state.isDisplay ? { display: 'none' } : {} } >
+              <div className="modal-header">
+                <h4 className="modal-title">Modal title</h4>
+              </div>
+              <div className="modal-body">One fine body...</div>
+              <div className="modal-footer">
+              <button type="button" className="btn btn-default">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
             </div>
         </div>
       );
